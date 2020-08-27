1b513e622c2a5076f3ebf00889070cf6ba1f023a
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-27 14:10:48 +0700

    Logging connected clients

--- src/socket.js
+++ src/socket.js
@@ -9,7 +9,7 @@ function getRandomLocation() {
 
 module.exports = (server) => {
 	const io = SocketIO(server);
-
+	const clients = {};
 	const gameState = {
 		poopCollected: 0,
 		animals: [
@@ -79,7 +79,11 @@ module.exports = (server) => {
 		],
 	};
 
+	let hasUpdate = false;
 	io.on("connection", (socket) => {
+		console.log("Connected clients: ", Object.keys(clients).length);
+		clients[socket.id] = true;
+		socket.emit("game-state", gameState);
 		socket.on("poop-collected", ({ id }) => {
 			gameState.poopCollected += 1;
 			gameState.poops = gameState.poops.filter((poop) => poop.id !== id);
@@ -87,10 +91,17 @@ module.exports = (server) => {
 				id: gameState.poops[gameState.poops.length - 1].id + 1,
 				location: getRandomLocation(),
 			});
+			hasUpdate = true;
+		});
+		socket.on("disconnect", () => {
+			delete clients[socket.id];
 		});
 	});
 
 	setInterval(() => {
-		io.emit("game-state", gameState);
-	}, 1000);
+		if (hasUpdate) {
+			io.emit("game-state", gameState);
+			hasUpdate = false;
+		}
+	}, 300);
 };
