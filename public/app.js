91cf6f6d1eea5944b1028d1d842f844cb78373b0
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-27 13:42:22 +0700

    Create array of emoji

--- public/app.js
+++ public/app.js
@@ -1,1 +1,47 @@
 const socket = io();
+
+const zoo = document.querySelector(".zoo");
+
+{
+	/* <span class="emoji animal bounce">🐅</span>
+<span class="emoji animal bounce">🐘</span>
+<span class="emoji poop">💩</span> */
+}
+
+const animalsById = {};
+const poopsById = {};
+
+function updateView(gameState) {
+	gameState.animals.forEach((animal) => {
+		if (!animalsById[animal.id]) {
+			const animalElement = document.createElement("span");
+			animalElement.classList.add("emoji");
+			animalElement.classList.add("animal");
+			animalElement.classList.add("bounce");
+			animalElement.textContent = animal.emoji;
+			animalsById[animal.id] = animalElement;
+			zoo.appendChild(animalElement);
+		}
+
+		animalsById[animal.id].style.top =
+			animal.location.y * window.innerHeight + "px";
+		animalsById[animal.id].style.left =
+			animal.location.x * window.innerWidth + "px";
+	});
+
+	gameState.poops.forEach((poop) => {
+		if (!poopsById[poop.id]) {
+			const poopsElement = document.createElement("span");
+			poopsElement.classList.add("emoji");
+			poopsElement.classList.add("poop");
+			poopsElement.textContent = "💩";
+			poopsById[poop.id] = poopsElement;
+			zoo.appendChild(poopsElement);
+		}
+
+		poopsById[poop.id].style.top = poop.y * window.innerHeight + "px";
+		poopsById[poop.id].style.left = poop.x * window.innerWidth + "px";
+	});
+}
+
+socket.on("game-state", updateView);
--- src/socket.js
+++ src/socket.js
@@ -3,7 +3,103 @@ const SocketIO = require("socket.io");
 module.exports = (server) => {
 	const io = SocketIO(server);
 
+	const gameState = {
+		animals: [
+			{
+				id: 0,
+				emoji: "🐅",
+				location: {
+					x: Math.random(),
+					y: Math.random(),
+				},
+			},
+			{
+				id: 1,
+				emoji: "🐘",
+				location: {
+					x: Math.random(),
+					y: Math.random(),
+				},
+			},
+			{
+				id: 2,
+				emoji: "🐎",
+				location: {
+					x: Math.random(),
+					y: Math.random(),
+				},
+			},
+			{
+				id: 3,
+				emoji: "🐧",
+				location: {
+					x: Math.random(),
+					y: Math.random(),
+				},
+			},
+			{
+				id: 4,
+				emoji: "🐿",
+				location: {
+					x: Math.random(),
+					y: Math.random(),
+				},
+			},
+		],
+		poops: [
+			{
+				id: 0,
+				x: Math.random(),
+				y: Math.random(),
+			},
+			{
+				id: 1,
+				x: Math.random(),
+				y: Math.random(),
+			},
+			{
+				id: 2,
+				x: Math.random(),
+				y: Math.random(),
+			},
+			{
+				id: 3,
+				x: Math.random(),
+				y: Math.random(),
+			},
+			{
+				id: 4,
+				x: Math.random(),
+				y: Math.random(),
+			},
+			{
+				id: 5,
+				x: Math.random(),
+				y: Math.random(),
+			},
+			{
+				id: 6,
+				x: Math.random(),
+				y: Math.random(),
+			},
+			{
+				id: 7,
+				x: Math.random(),
+				y: Math.random(),
+			},
+			{
+				id: 8,
+				x: Math.random(),
+				y: Math.random(),
+			},
+		],
+	};
+
 	io.on("connection", (socket) => {
 		console.log("a user connected");
 	});
+
+	setInterval(() => {
+		io.emit("game-state", gameState);
+	}, 1000);
 };
